7e0bfd0e334600d382e1b1eff7f40de11215ca3b
Sergey Stepanenko <nbo1995@gmail.com>
2020-06-08 09:44:17 +0400

    Создал переменные и изменил сообщение alert

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
-alert('hello');
-[1,2,3].push()
+alert('Привет, меня зовут Сергей Степаненко и я изучаю JS на курсе glo academy');
+
+var money = 10;
+var income = 12;
+var iaddExpenses = 1;
+var deposit = 1000;
+var mission = 'Михаил поедит на море, но как нибудь потом';
+var period = 'Михаил';
